853a12e776f64ca2a0602dc815bdd7b2d59ca0fe
Regina Ragnarsdottir <reginaragnarsd@gmail.com>
2021-08-31 21:18:57 +0000

    home screen colors and buttons

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:movie_stream/screens/get_latest_widget.dart';
+import 'package:movie_stream/screens/movie_category.dart';
 import 'package:movie_stream/screens/now_playing_widget.dart';
 import 'package:movie_stream/screens/profile_screen.dart';
 
@@ -50,6 +51,24 @@ class HomeScreen extends StatelessWidget {
                 height: 40.0,
               ),
               GetLatest(),
+              SizedBox(
+                height: 40.0,
+              ),
+              Container(
+                width: double.infinity,
+                height: 50.0,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(primary: Colors.black),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                        builder: (context) => MovieCategory(),
+                    )
+                    );},
+                  child: Text('Explore All', style: TextStyle(fontSize: 18.0),),
+                ),
+              ),
             ],
           ),
         ),
